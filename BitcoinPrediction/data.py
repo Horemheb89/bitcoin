ed691e3d26703bf2b617bf66a2d67b843f46c768
Caroline Trenet <horemheb89@yahoo.co.uk>
2021-03-15 12:52:30 +0100

    prepared repo for individual work

--- BitcoinPrediction/data.py
+++ BitcoinPrediction/data.py
@@ -2,10 +2,13 @@ from google.cloud import storage
 import pandas as pd
 
 def get_data():
-    """method to get the training data (or a portion of it) from google cloud bucket"""
+    """method to get the training data (or a portion of it) from local folder (or google cloud bucket)"""
     # client = storage.Client()
     # data = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}")
     data = pd.read_csv('../raw_data/bitstampUSD.csv')
     # data = pd.read_csv('gs://bitcoin-prediction-01/data/bitstampUSD.csv')
     return data
 
+# def clean_data():
+"""method to prepare the data for analysis"""
+#     pass
